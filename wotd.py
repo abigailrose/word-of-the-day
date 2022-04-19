8cc4636797de04bbb3fba66ce9515ac1aee22dfd
Abigail Fox <abigailfireflyfox@gmail.com>
2022-04-19 17:51:37 -0400

    added words

--- wotd.py
+++ wotd.py
@@ -5,4 +5,4 @@ words = open("words.txt", 'r')
 word_list = words.readlines()
 index = random.randint(0, len(word_list))
 
-print("The word of the day is: {}".format(word_list[index]))
+print("\nThe word of the day is: {}".format(word_list[index]))
